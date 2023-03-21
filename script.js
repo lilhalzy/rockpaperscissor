3c205fa6acd5926ab884a4e2241cb77835e6e53d
lilhalzy <lilhalzy11@gmail.com>
2023-03-21 09:54:52 +0800

    feat: displaying weapon UI

--- script.js
+++ script.js
@@ -23,7 +23,6 @@ function playRound(humanSelection, aiSelection) {
     aiScore++;
     roundWinner = 'AI';
   }
-  updateScoreMsg(roundWinner, humanSelection, aiSelection);
 }
 
 // Generate random weapon
@@ -41,4 +40,64 @@ function roulette() {
 
 function gameOver() {
   return humanScore === 5 || aiScore === 5;
+}
+// UI
+
+const scoreInfo = document.querySelector('#scoreInfo');
+const scoreMessage = document.querySelector('#scoreSubtitle');
+const humanScorePara = document.querySelector('#humanScore');
+const aiScorePara = document.querySelector('#aiScore');
+const humanSign = document.querySelector('#humanSign');
+const aiSign = document.querySelector('#aiSign');
+const rockBtn = document.querySelector('#rockBtn');
+const paperBtn = document.querySelector('#paperBtn');
+const scissorsBtn = document.querySelector('#scissorsBtn');
+
+rockBtn.addEventListener('click', () => {
+  handleClick('ROCK');
+});
+paperBtn.addEventListener('click', () => {
+  handleClick('PAPER');
+});
+scissorsBtn.addEventListener('click', () => {
+  handleClick('SCISSORS');
+});
+
+
+function handleClick(humanSelection) {
+  if (gameOver()) {
+    openEndGameModal();
+    return;
+  }
+
+  const aiSelection = roulette();
+  playRound(humanSelection, aiSelection);
+  updateChoices(humanSelection, aiSelection);
+  updateScore();
+
+}
+
+function updateChoices(humanSelection, aiSelection) {
+  switch (humanSelection) {
+    case 'ROCK':
+      humanSign.textContent = '🪨';
+      break;
+    case 'PAPER':
+      humanSign.textContent = '📃';
+      break;
+    case 'SCISSORS':
+      humanSign.textContent = '✂️';
+      break;
+  }
+  switch (aiSelection) {
+    case 'ROCK':
+      aiSign.textContent = '🪨';
+      break;
+    case 'PAPER':
+      aiSign.textContent = '📃';
+      break;
+    case 'SCISSORS':
+      aiSign.textContent = '✂️';
+      break;
+  }
 }
